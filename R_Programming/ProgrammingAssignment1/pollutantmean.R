2a24f0be9e5915908bfc9a5c5e1b4b6d22f70490
bxu <xu.bentony@gmail.com>
2015-10-15 16:26:23 -0500

    add comments to cachematrix.R

--- R_Programming/ProgrammingAssignment1/pollutantmean.R
+++ R_Programming/ProgrammingAssignment1/pollutantmean.R
@@ -1,13 +1,14 @@
 pollutantmean <- function(directory, pollutant, id=1:332) {
-    retMean <- 0.0
+    ret <- c()
     for (monitor in id) {
         if (monitor < 10)      location <- paste(c(directory, '/', '00', monitor, '.csv'), collapse = '')
         else if(monitor < 100) location <- paste(c(directory, '/', '0',  monitor, '.csv'), collapse = '')
         else                   location <- paste(c(directory, '/',       monitor, '.csv'), collapse = '')
         
         data <- read.csv(location)
-        retMean <- retMean + colMeans(data[pollutant], na.rm=TRUE)
+        # concatenate the data
+        ret <- c(ret, na.omit(data[[pollutant]]))
     }
     
-    retMean / length(id)
+    mean(ret)
 }
--- R_Programming/ProgrammingAssignment2/cachematrix.R
+++ R_Programming/ProgrammingAssignment2/cachematrix.R
@@ -2,7 +2,18 @@
 ## functions do
 
 ## Write a short comment describing this function
+## utilize the <<- operator which can be used to assign a value to an object in an
+## environment that is different from the current environment. 
 
+## The function is able to cache potentially time-consuming computations, here, the
+## inverse of an matrix. If the contents of a vector are not changing, it caches the
+## value so that when we need it again, it can be looked up in the cache rather than
+## recomputed. 
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+##      above. If the inverse has already been calculated (and the matrix has not changed), then
+##      the cachesolve should retrieve the inverse from the cache.
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
